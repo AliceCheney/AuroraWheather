536836ec94d36f96212fcd1dd7b8109572a1eae6
Aurora <cheneyjie@qq.com>
2019-08-08 22:02:22 +0800

    2019/8/8/22:02

--- js/index.js
+++ js/index.js
@@ -1,46 +1,25 @@
-
 $(document).ready(function () {
-
-    setInterval("nowTime()",1000);
-
+    let time=new Date();
+    let month=time.getMonth()+1;
+    let day=time.getDate();
+    //获取时分秒
+    let h=time.getHours();
+    let m=time.getMinutes();
+   $('#nowTime').text( month+"/"+day+" ，"+h+":"+m);
     $('.select2').select2({
-
     });
 });
-    var clickSelectForm = false;
+let cd = 30;
+let isCd = true;
+let clickSelectForm = false;
 
 $('#areaLocalA').click(function () {
-
     if (clickSelectForm) {
-
         $('#selectForm').hide()
         clickSelectForm = false;
     }else {
-
         $('#selectForm').show()
         clickSelectForm = true;
-
     }
 });
-function nowTime(){
-    //获取月日
-    var time=new Date();
-    var month=time.getMonth()+1;
-    var day=time.getDate();
-    //获取时分秒
-    var h=time.getHours();
-    var m=time.getMinutes();
-    //检查是否小于10
-    h=check(h);
-    m=check(m);
-    document.getElementById("nowTime").innerHTML= month+"/"+day+" ，"+h+":"+m;
-}
-//"当前时间："+year+"年"+month+"月"+day+"日  "+h+":"+m+":"+s;
-//时间数字小于10，则在之前加个“0”补位。
-function check(i){
-    //方法一，用三元运算符
-    var num;
-    i<10?num="0"+i:num=i;
-    return num;
-}
 
