a4da0a4271cd36f2f4944d68e388da8ecb557937
Aurora <cheneyjie@qq.com>
2019-08-12 22:16:14 +0800

    2019/8/12/22:16

--- js/index.js
+++ js/index.js
@@ -8,27 +8,33 @@ $(document).ready(function () {
    $('#nowTime').text( month+"/"+day+" ，"+h+":"+m);
     $('.select2').select2({
     });
-});
-let cd = 30;
-let isCd = true;
 
-$('#areaLocalDetail').click(function () {
-    if ($('#selectForm').css('visibility') === 'visible'){
-        $('#selectForm').css('visibility','hidden')
-    } else {
-        $('#selectForm').css('visibility','visible')
-    }
-});
-let isCf = false;
-$('#clickC').click(function () {
-    if (isCf){
-        $('#clickF').text('F')
-        $('#clickC').text('C')
-        isCf = false;
-    }else {
-        $('#clickF').text('C')
-        $('#clickC').text('F')
-        // f=c×9/5+32
-        isCf = true;
-    }
+    let ct = 30;
+    let fc = ct*9/5+32;
+
+    $('#temperature').text(ct+'°');
+
+    $('#areaLocalDetail').click(function () {
+        if ($('#selectForm').css('visibility') === 'visible'){
+            $('#selectForm').css('visibility','hidden')
+        } else {
+            $('#selectForm').css('visibility','visible')
+        }
+    });
+
+    let isC = true;
+    $('#clickBottom').click(function () {
+        if (isC){
+            $('#clickTop').text('F')
+            $('#clickBottom').text('C')
+            $('#temperature').text(fc+'°')
+            isC = false;
+        }else {
+            $('#clickTop').text('C')
+            $('#clickBottom').text('F')
+            // f=c×9/5+32
+            $('#temperature').text(ct+'°')
+            isC = true;
+        }
+    });
 });
