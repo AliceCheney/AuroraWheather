9847ed2d52decb018afd85706a6e15a307db9d99
AliceCheney <Cheneyjie@qq.com>
2019-08-21 19:10:59 +0800

    2019/8/21/19:10

--- js/index.js
+++ js/index.js
@@ -39,13 +39,76 @@ $(document).ready(function () {
     });
 
     $.ajax({
-        url:"https://www.tianqiapi.com/api/?version=v1&cityid=101120609",
+        url:"https://www.tianqiapi.com/api/?version=v1&cityid=101100000",
         type:"get",
-        success:function (result) {
-            console.log(result)
+        success:function (success) {
+            console.log(success.data);
+            $('#week1').text(success.data[0].week);
+            $('#weatherText1').text(success.data[0].air_tips);
+            $('#maxTemperature1').text(success.data[0].tem1);
+            $('#minTemperature1').text(success.data[0].tem2);
+        //    2
+            $('#week2').text(success.data[1].week);
+            $('#weatherText2').text(success.data[1].index[1].desc);
+            $('#maxTemperature2').text(success.data[1].tem1);
+            $('#minTemperature2').text(success.data[1].tem2);
+        //    3
+            $('#week3').text(success.data[2].week);
+            $('#weatherText3').text(success.data[2].index[1].desc);
+            $('#maxTemperature3').text(success.data[2].tem1);
+            $('#minTemperature3').text(success.data[2].tem2);
+        //    4
+            $('#week4').text(success.data[3].week);
+            $('#weatherText4').text(success.data[3].index[1].desc);
+            $('#maxTemperature4').text(success.data[3].tem1);
+            $('#minTemperature4').text(success.data[3].tem2);
         },
         error:function (error) {
             console.log(error.statusText)
         }
     })
+});
+    let weather1 = false;
+    let weather2 = false;
+    let weather3 = false;
+    let weather4 = false;
+    let weather5 = false;
+    let weather6 = false;
+    let weather7 = false;
+    $('#clickWeatherDayDetails1').click(function () {
+        if (weather1){
+            $('.weatherStyle').css('display','none');
+            weather1 = false;
+        }else {
+            $('.weatherStyle').css('display','block');
+            weather1 = true;
+        }
+    });
+
+    $('#clickWeatherDayDetails2').click(function () {
+        if (weather2){
+            $('#weatherStyle2').css('display','none');
+            weather2 = false;
+        }else {
+            $('#weatherStyle2').css('display','block');
+            weather2 = true;
+        }
+    });
+    $('#clickWeatherDayDetails3').click(function () {
+        if (weather3){
+            $('#weatherStyle3').css('display','none');
+            weather3 = false;
+        }else {
+            $('#weatherStyle3').css('display','block');
+            weather3 = true;
+        }
+    });
+$('#clickWeatherDayDetails4').click(function () {
+    if (weather4){
+        $('#weatherStyle4').css('display','none');
+        weather4 = false;
+    }else {
+        $('#weatherStyle4').css('display','block');
+        weather4 = true;
+    }
 });
